2ab03ce632310a9137c6507acd0a675ec1479ee6
mrdavidhidalgo <d.hidalgo@uniandes.edu.co>
2022-10-30 23:53:53 -0500

    Se agrega seguridad de kafka para la nube

--- converter-api/adapters/conversion_scheduler.py
+++ converter-api/adapters/conversion_scheduler.py
@@ -5,14 +5,27 @@ from typing import List
 from json import dumps
 from kafka import KafkaProducer,KafkaConsumer
 from  services import logs 
+import os
 log = logs.get_logger()
 class FileConversionScheduler(file_conversion_scheduler.FileConversionScheduler):
 
+
     def __init__(self):
-        self.producer = KafkaProducer(bootstrap_servers=['kafka:9092'],
+        bootstrap_servers = os.getenv("bootstrap_servers","kafka:9092")
+        sasl_plain_username= os.getenv("sasl_plain_username","user")#se debe colocar el user
+        sasl_plain_password=os.getenv("sasl_plain_password","kTm5cVvQ4reC")#se debe especificar contraseña
+        security_protocol=os.getenv("security_protocol","SASL_PLAINTEXT")
+        sasl_mechanism=os.getenv("sasl_mechanism","PLAIN")
+        
+        
+        self.producer = KafkaProducer(bootstrap_servers=bootstrap_servers,
+                         sasl_plain_username=sasl_plain_username, sasl_plain_password=sasl_plain_password,
+                         security_protocol=security_protocol, sasl_mechanism=sasl_mechanism,
                          value_serializer=lambda x: 
                          x.json().encode('utf-8'),
                          )
+        
+        
     def schedule_conversion_task(self, conversion_task: ConversionTaskDetail) -> None:
         
         log.info("Send task with id [%s] to Kafka topic [%s] message -> %s",conversion_task.id,'file.conversion.requested', conversion_task.json())
